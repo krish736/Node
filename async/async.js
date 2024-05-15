584825203fb9785b020401da68cae7eb28273c67
krish736 <112505529+krish736@users.noreply.github.com>
2024-05-15 15:45:14 +0530

    commit

--- async/async.js
+++ async/async.js
@@ -3,15 +3,15 @@ const { readFile, writeFile } = require("fs").promises;
 const start = async () => {
   try {
     const first = await readFile(
-      "./built-in_modules/pathcheck/subfolder/first.txt",
+      "../built-in_modules/pathcheck/subfolder/first.txt",
       "utf8"
     );
     const second = await readFile(
-      "./built-in_modules/pathcheck/subfolder/second.txt",
+      "../built-in_modules/pathcheck/subfolder/second.txt",
       "utf8"
     );
     await writeFile(
-      "./built-in_modules/pathcheck/subfolder/async_await.txt",
+      "../built-in_modules/pathcheck/subfolder/async_await.txt",
       "This is how to use async await!"
     );
     console.log(first, second);
